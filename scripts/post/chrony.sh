e7b2de08113166b34118a94bc2d2558fe95aa333
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-08-31 09:44:10 +0000

    Update chrony.sh

--- scripts/post/chrony.sh
+++ scripts/post/chrony.sh
@@ -1,8 +1,8 @@
-wget -P /usr/etc https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf
+#!/usr/bin/env bash
 
-systemctl disable NetworkManager-wait-online.service && \
-systemctl mask rpm-ostree-countme.timer && \
-systemctl enable com.system76.Scheduler.service && \
-systemctl enable bazzite-hardware-setup.service && \
-systemctl unmask dconf-update.service && \
-systemctl enable dconf-update.service
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+wget -P /usr/etc https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf
