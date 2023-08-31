b51e5871c6281aa6c9f3906b895a24fbc875304a
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-08-31 09:43:14 +0000

    Update bazzite.sh

--- scripts/post/bazzite.sh
+++ scripts/post/bazzite.sh
@@ -1,12 +1,9 @@
- && \
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
 wget -P /usr/etc/sysctl.d https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/etc/sysctl.d/30-networking.conf && \
 wget -P /usr/bin/ https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/bin/bazzite-hardware-setup
-
-wget -P /usr/etc https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf
-
-systemctl disable NetworkManager-wait-online.service && \
-systemctl mask rpm-ostree-countme.timer && \
-systemctl enable com.system76.Scheduler.service && \
-systemctl enable bazzite-hardware-setup.service && \
-systemctl unmask dconf-update.service && \
-systemctl enable dconf-update.service
