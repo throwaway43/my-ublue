0fd9b732bcfc07aa3d2a5e2266be1fa01c6151d5
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-08-31 09:44:58 +0000

    Update 99-services.sh

--- scripts/post/99-services.sh
+++ scripts/post/99-services.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
 
 systemctl disable NetworkManager-wait-online.service && \
 systemctl mask rpm-ostree-countme.timer && \
