cc5df0659d29b70698e106096752e27ee9715bdd
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-08-31 09:40:57 +0000

    Update and rename oneminute to kicksecure.sh

--- scripts/post/kicksecure.sh
+++ scripts/post/kicksecure.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
 
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'This is an example shell script'
+echo 'Scripts here will run during build if specified in recipe.yml'
 # Cleanup & Finalize
 
 RUN wget -P /usr/etc/sysctl.d https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc.conf && \
