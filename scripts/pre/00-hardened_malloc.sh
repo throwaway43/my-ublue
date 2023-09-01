90244d0215468d3a3f68442827489bf2692c75d3
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-01 16:30:07 +0000

    Update 00-hardened_malloc.sh

--- scripts/pre/00-hardened_malloc.sh
+++ scripts/pre/00-hardened_malloc.sh
@@ -5,7 +5,7 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-sudo wget -O /etc/yum.repos.d/divested-release.repo https://gitlab.com/divested/divested-release/-/raw/master/divested-release.repo\?ref_type\=heads
-sudo wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-divested https://gitlab.com/divested/divested-release/-/raw/master/RPM-GPG-KEY-divested?ref_type=heads
+wget -O /etc/yum.repos.d/divested-release.repo https://gitlab.com/divested/divested-release/-/raw/master/divested-release.repo\?ref_type\=heads
+wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-divested https://gitlab.com/divested/divested-release/-/raw/master/RPM-GPG-KEY-divested?ref_type=heads
 rpm-ostree install hardened_malloc
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/divested-release.repo
