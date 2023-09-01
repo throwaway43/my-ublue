60af0470ed04c29b6ea9fddc4483636dd176f82f
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-01 18:59:48 +0000

    Update 00-fstab.sh

--- scripts/pre/00-fstab.sh
+++ scripts/pre/00-fstab.sh
@@ -1,1 +1,9 @@
-sed
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+sed -i 's@defaults@defaults,nodev,noexec,nosuid@g' /etc/fstab
+sed -i 's@winnt@winnt,nodev,noexec,nosuid@g' /etc/fstab
