a7d56fcace5d95e4d504a73841a0047a09e1d8ac
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-01 16:14:04 +0000

    Create 00-hardened_malloc.sh

---
+++ scripts/pre/00-hardened_malloc.sh
@@ -0,0 +1,4 @@
+RUN wget -O /etc/yum.repos.d/divested-release.repo https://gitlab.com/divested/divested-release/-/raw/master/divested-release.repo\?ref_type\=heads && \
+        wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-divested https://gitlab.com/divested/divested-release/-/raw/master/RPM-GPG-KEY-divested?ref_type=heads && \
+        rpm-ostree install hardened_malloc && \
+        sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/divested-release.repo
