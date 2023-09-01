942406ac1d1d79679d4ddb6c9d2ccc2ac9ac0089
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-01 13:46:00 +0000

    Update 02-divested.sh

--- scripts/pre/02-divested.sh
+++ scripts/pre/02-divested.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
 sudo wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-divested https://gitlab.com/divested/divested-release/-/raw/master/RPM-GPG-KEY-divested?ref_type=heads
