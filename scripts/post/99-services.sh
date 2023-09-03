56343e3e9b79de3c7cb215117b95589bb399ec5c
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-03 18:50:43 +0000

    Update 99-services.sh

--- scripts/post/99-services.sh
+++ scripts/post/99-services.sh
@@ -8,6 +8,5 @@ set -oue pipefail
 systemctl disable NetworkManager-wait-online.service && \
 systemctl mask rpm-ostree-countme.timer && \
 systemctl enable com.system76.Scheduler.service && \
-systemctl enable bazzite-hardware-setup.service && \
 systemctl unmask dconf-update.service && \
 systemctl enable dconf-update.service
