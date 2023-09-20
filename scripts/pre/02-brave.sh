349180441f34635c07f49972b1cc5918107919b6
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-20 15:15:33 +0200

    Update 02-brave.sh

--- scripts/pre/02-brave.sh
+++ scripts/pre/02-brave.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
