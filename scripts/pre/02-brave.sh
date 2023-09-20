3ec1823efe0693791b614da91307ffacbb629501
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-20 16:08:32 +0200

    Update 02-brave.sh

--- scripts/pre/02-brave.sh
+++ scripts/pre/02-brave.sh
@@ -6,6 +6,7 @@
 set -oue pipefail
 
 wget -O /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+echo 'gpgcheck=1' | tee -a /etc/yum.repos.d/brave-browser.repo 1>/dev/null
 echo 'gpgkey=https://brave-browser-rpm-release.s3.brave.com/brave-core.asc' | tee -a /etc/yum.repos.d/brave-browser.repo 1>/dev/null
 rpm-ostree install brave-keyring brave-browser
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/brave-browser.repo
