bf3de9cc054db860c96266a4ad326fd4929f15d2
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-20 15:16:18 +0200

    Update 02-brave.sh

--- scripts/pre/02-brave.sh
+++ scripts/pre/02-brave.sh
@@ -5,4 +5,4 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
