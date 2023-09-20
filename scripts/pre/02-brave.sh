91754130c8e1f71642878497ee99b9aa3d1e860b
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-09-20 16:34:43 +0200

    Update 02-brave.sh

--- scripts/pre/02-brave.sh
+++ scripts/pre/02-brave.sh
@@ -6,9 +6,10 @@
 set -oue pipefail
 
 wget -O /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-wget -O /etc/pki/rpm-gpg/brave.asc https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+#wget -O /etc/pki/rpm-gpg/brave.asc https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 echo 'gpgcheck=1' | tee -a /etc/yum.repos.d/brave-browser.repo 1>/dev/null
 echo 'repo_gpgcheck=1' | tee -a /etc/yum.repos.d/brave-browser.repo 1>/dev/null
-echo 'gpgkey=file:///etc/pki/rpm-gpg/brave-core.asc' | tee -a /etc/yum.repos.d/brave-browser.repo 1>/dev/null
-rpm-ostree install brave-keyring brave-browser
+echo 'gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-brave' | tee -a /etc/yum.repos.d/brave-browser.repo 1>/dev/null
+rpm-ostree install brave-keyring
+rpm-ostree install brave-browser
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/brave-browser.repo
