ef55c10eff1fdcfd890c8b42591f28820a400b4f
throwaway43 <108981964+throwaway43@users.noreply.github.com>
2023-11-23 12:22:32 +0000

    Update 90-mozilla-gnome-themes-user-update.sh

--- scripts/post/90-mozilla-gnome-themes-user-update.sh
+++ scripts/post/90-mozilla-gnome-themes-user-update.sh
@@ -6,5 +6,5 @@
 set -oue pipefail
 
 mkdir -p /etc/ublue-update.d
-wget -O /etc/ublue-update.d/90-mozilla-gnome-themes-user-update.sh https://raw.githubusercontent.com/ublue-os/bazzite/ddebbd3f2f761f1bd1c0dcae5b42902f5b91a2bf/system_files/desktop/silverblue/etc/ublue-update.d/90-mozilla-gnome-themes-user-update.sh
+wget -O /etc/ublue-update.d/90-mozilla-gnome-themes-user-update.sh https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/silverblue/usr/etc/ublue-update.d/user/90-mozilla-gnome-themes-user-update.sh
 sed -i 's|org.mozilla.firefox/.mozilla/firefox/|io.gitlab.librewolf-community/.librewolf/|g' /etc/ublue-update.d/90-mozilla-gnome-themes-user-update.sh
